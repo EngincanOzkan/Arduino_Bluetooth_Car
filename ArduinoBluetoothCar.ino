6141b02ccaf3609ff2456353230a29412b36835f
Engincan Özkan <engincanozkan@Engincan-MacBook-Pro.local>
2018-04-01 20:04:43 +0300

    Added Android Files

--- ArduinoBluetoothCar.ino
+++ ArduinoBluetoothCar.ino
@@ -1,65 +1,64 @@
 #include <SoftwareSerial.h>
-SoftwareSerial Bluetooth(10, 11); // RX, TX
+SoftwareSerial Bluetooth(3, 4);
 
-int izqA = 5;
-int izqB = 6;
-int derA = 8;
-int derB = 9;
-int vel = 255; // Velocidad de los motores (0-255)
-int gelenVeri = '7'; // inicia detenido
+int izqA = 5; 
+int izqB = 6; 
+int derA = 8; 
+int derB = 9; 
+int deger = 255;
 
-void setup() {
-  Bluetooth.begin(9600);
-  Serial.begin(9600);  //Bluetooth için bu porta ile connection veriyoruz
-  Serial.println("Waiting for command...");
-  pinMode(derA, OUTPUT);
-  pinMode(derB, OUTPUT);
-  pinMode(izqA, OUTPUT);
-  pinMode(izqB, OUTPUT);
-}
+int data = '3';
+
+void setup() { 
+Bluetooth.begin(9600);   
+Serial.begin(9600);
+Serial.println("Waiting for command...");
+Bluetooth.println("Send 1 to turn on the LED. Send 0 to turn Off");
+pinMode(derA, OUTPUT);
+pinMode(derB, OUTPUT);
+pinMode(izqA, OUTPUT);
+pinMode(izqB, OUTPUT);
+
+pinMode(LED_BUILTIN, OUTPUT);
+
+} 
 
 void loop() {
 
-  if (Bluetooth.available()) { //bluetooth bağlanmışsa
-    gelenVeri = Bluetooth.read();
- 
+    if(Bluetooth.available())
+        { data=Bluetooth.read();
+        }
 
-  if (gelenVeri == '1') { // sag
-    analogWrite(derB, 0);
-    analogWrite(izqB, 0);
-    analogWrite(derA, vel);
-    analogWrite(izqA, vel);
-  }
-  if (gelenVeri == '2') { // ileri
-    analogWrite(derB, vel);
-    analogWrite(izqB, 0);
-    analogWrite(derA, 0);
-    analogWrite(izqA, vel);
-  }
-  if (gelenVeri == '3') { // Stop
-    analogWrite(derB, 0);
-    analogWrite(izqB, 0);
-    analogWrite(derA, 0);
-    analogWrite(izqA, 0);
-  }
-  if (gelenVeri == '4') { // geri
-    analogWrite(derB, 0);
-    analogWrite(izqB, vel);
-    analogWrite(izqA, 0);
-    analogWrite(derA, vel);
-  }
+if(data == '1') {//ileri
+analogWrite(derB, 0); 
+analogWrite(izqB, deger); 
+analogWrite(derA, deger); 
+analogWrite(izqA, 0);
 
-  if (gelenVeri == '5') { // sol
-    analogWrite(derA, 0);
-    analogWrite(izqA, 0);
-    analogWrite(derB, vel);
-    analogWrite(izqB, vel);
-  }
-  if (gelenVeri == '6') { 
+}else if(data == '2'){
+analogWrite(derB, 0); 
+analogWrite(izqB, 0); 
+analogWrite(derA, deger); 
+analogWrite(izqA, deger);
 
-  }
-  if (gelenVeri == '7') {
+}else if(data == '3'){
+analogWrite(derB, 0); 
+analogWrite(izqB, 0); 
+analogWrite(derA, 0); 
+analogWrite(izqA, 0); 
 
-  }
+}else if(data == '4'){
+analogWrite(derB, deger); 
+analogWrite(izqB, deger); 
+analogWrite(derA, 0); 
+analogWrite(izqA, 0); 
+
+}else if(data == '5'){
+analogWrite(derB, deger); 
+analogWrite(izqB, 0); 
+analogWrite(derA, 0); 
+analogWrite(izqA, deger); 
+
 }
+    
 }
